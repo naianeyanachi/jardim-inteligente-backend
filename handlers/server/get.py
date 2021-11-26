14a6a2da6365390556c51e61a60d76fd45655110
naianeyanachi <naiane.yanachi@gmail.com>
2021-11-26 17:13:30 -0300

    iso format

--- handlers/server/get.py
+++ handlers/server/get.py
@@ -4,4 +4,4 @@ from utils.response import OK
 
 
 def handler(event, context):
-    return OK(datetime.datetime.utcnow.strftime('%B %d %Y - %H:%M:%S'))
+    return OK(datetime.datetime.utcnow.isoformat())
